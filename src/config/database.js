c57e27900e2751113de420a6ab9bc85272da3d80
Leandro <leandro.lanzoni0400@gmail.com>
2025-04-27 19:42:37 -0300

    Atualização para Deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('bdecomolk', 'root', 'Ot2004**', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
+const sequelize = new Sequelize( 
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
+    {
+        port: process.env.DB_PORT,
+        host: process.env.DB_HOST,
+        dialect: 'mysql'
+    }
+);
 
 module.exports = sequelize;
