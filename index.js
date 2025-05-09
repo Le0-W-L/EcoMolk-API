c290428930eb83f9e22533984e15b2b2097cfbc4
Leandro <leandro.lanzoni0400@gmail.com>
2025-05-09 12:02:16 -0300

    Alteração no Caminho da Landing Page

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname, "src", "views")));
 
 // Rota da Landing Page
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "src", "views", "LandingPage", "landingPage.html"))
+  res.sendFile(path.join(__dirname, "src", "views", "LandingPage", "LandingPage_index.html"))
 );
 
 // Inicialização do servidor
